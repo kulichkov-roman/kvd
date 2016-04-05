4111501911b381105fc998177b605e6556185a55
Kulichkov Roman <roman@kulichkov.pro>
2016-04-06 02:02:52 +0600

    #config актуализирован конфиг

--- local/php_interface/config/common.php
+++ local/php_interface/config/common.php
@@ -5,13 +5,7 @@
 \Quetzal\Environment\EnvironmentManager::getInstance()->addConfig(
 	new \Quetzal\Environment\Configuration\CommonConfiguration(
 		array(
-			// id ИБ "Слайдер"
 			'sliderMainIBlockId' => 1,
-			// id ИБ "Обратная связь"
-			'feedbackIBlockId' => 7,
-			// id ИБ "Контакты"
-			'contactsSliderIBlockId' => 8,
-			// заглушка для слайдера
 			'sliderMainPlugId' => 3,
 		)
 	)

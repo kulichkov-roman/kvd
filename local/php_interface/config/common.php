2efbd4eee299550288347ed4916f8e6f68b096af
Roman Kulichkov <roman@kulichkov.pro>
2016-04-05 18:04:56 +0600

    Revert "#migration создал тип ИБ и ИБ слайдер."
    This reverts commit 4d0341ba8051dd8026ce4435ca8d2b861f2e18ad.

--- local/php_interface/config/common.php
+++ local/php_interface/config/common.php
@@ -5,10 +5,22 @@
 \Quetzal\Environment\EnvironmentManager::getInstance()->addConfig(
 	new \Quetzal\Environment\Configuration\CommonConfiguration(
 		array(
+			/**
+			 * @todo нужно выполнить миграцию 002_create_hero_slider_iblock.php
+			 * и записать сюда полученный id
+			 */
 			// id ИБ "Слайдер"
-			'sliderMainIBlockId' => 1,
+			'sliderHeroIBlockId' => 6,
+			/**
+			 * @todo нужно выполнить миграцию 004_create_feedback_iblock.php
+			 */
 			// id ИБ "Обратная связь"
 			'feedbackIBlockId' => 7,
+			'feedbackEmailPropId' => 8,
+			'feedbackPhonePropId' => 9,
+			/**
+			 * @todo нужно выполнить миграцию 006_create_contacts_slider_iblock.php
+			 */
 			// id ИБ "Контакты"
 			'contactsSliderIBlockId' => 8,
 		)

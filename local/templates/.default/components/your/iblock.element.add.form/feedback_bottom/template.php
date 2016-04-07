6eb18d0e4095a9d0fecb6543b58fcdd518766f57
Roman Kulichkov <roman@kulichkov.pro>
2016-04-07 15:28:53 +0600

    Правка вёрстки шаблона нижней формы обратной связи http://local.prostosait.ru/company/personal/user/486/tasks/task/view/3439/

--- local/templates/.default/components/your/iblock.element.add.form/feedback_bottom/template.php
+++ local/templates/.default/components/your/iblock.element.add.form/feedback_bottom/template.php
@@ -38,40 +38,42 @@ if (strlen($arResult["MESSAGE"]) > 0) {
 }
 ?>
 <section class="form-section">
-	<h2>Оставить заявку</h2>
-	<form name="iblock_add" action="<?=POST_FORM_ACTION_URI?>" method="post" enctype="multipart/form-data">
-		<?=bitrix_sessid_post()?>
-		<?if ($arParams["MAX_FILE_SIZE"] > 0){?>
-			<input type="hidden" name="MAX_FILE_SIZE" value="<?=$arParams["MAX_FILE_SIZE"]?>" />
-		<?}?>
-		<div class="form-block">
-			<div class="row">
-				<div class="col-3">
-					<div class="field">
-						<label for="">Имя</label>
-						<input name="PROPERTY[NAME][0]" type="text">
+	<div class="wrap">
+		<h2>Оставить заявку</h2>
+		<form name="iblock_add" action="<?=POST_FORM_ACTION_URI?>" method="post" enctype="multipart/form-data">
+			<?=bitrix_sessid_post()?>
+			<?if ($arParams["MAX_FILE_SIZE"] > 0){?>
+				<input type="hidden" name="MAX_FILE_SIZE" value="<?=$arParams["MAX_FILE_SIZE"]?>" />
+			<?}?>
+			<div class="form-block">
+				<div class="row">
+					<div class="col-3">
+						<div class="field">
+							<label for="">Имя</label>
+							<input name="PROPERTY[NAME][0]" type="text">
+						</div>
+					</div>
+					<div class="col-3">
+						<div class="field">
+							<label for="">Телефон</label>
+							<input name="PROPERTY[<?=$environment->get('feedbackPropPhoneId')?>][0]" type="text" class="phone-mask">
+						</div>
+					</div>
+					<div class="col-3">
+						<div class="field ">
+							<label for="">Эл.почта</label>
+							<input name="PROPERTY[<?=$environment->get('feedbackPropEmailId')?>][0]" type="text">
+						</div>
 					</div>
 				</div>
-				<div class="col-3">
-					<div class="field">
-						<label for="">Телефон</label>
-						<input name="PROPERTY[<?=$environment->get('feedbackPropPhoneId')?>][0]" type="text" class="phone-mask">
-					</div>
+				<div class="field">
+					<label for="">Комментарий</label>
+					<textarea name="PROPERTY[PREVIEW_TEXT][0]" cols="30" rows="10"></textarea>
 				</div>
-				<div class="col-3">
-					<div class="field ">
-						<label for="">Эл.почта</label>
-						<input name="PROPERTY[<?=$environment->get('feedbackPropEmailId')?>][0]" type="text">
-					</div>
+				<div class="field-btn">
+					<input type="submit" name="<?=$arParams['PREFIX_FORM']?>_iblock_submit" value="Отправить" />
 				</div>
 			</div>
-			<div class="field">
-				<label for="">Комментарий</label>
-				<textarea name="PROPERTY[PREVIEW_TEXT][0]" cols="30" rows="10"></textarea>
-			</div>
-			<div class="field-btn">
-				<input type="submit" name="<?=$arParams['PREFIX_FORM']?>_iblock_submit" value="Отправить" />
-			</div>
-		</div>
-	</form>
+		</form>
+	</div>
 </section>

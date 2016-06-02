8cc86806492207bc83e6f0d2dc569f87cbd678e9
Roman Kulichkov <roman@kulichkov.pro>
2016-06-02 17:57:17 +0600

    Синхронизировал папку со включаемыми областями

--- local/include/site_templates/hd_reviews.php
+++ local/include/site_templates/hd_reviews.php
@@ -1,42 +1,48 @@
 <section class="reviews-section">
-			<div class="reviews-bg"></div>
-			<div class="wrap">
-				<h2>Отзывы</h2>
-				<div class="reviews-block">
-					<div class="reviews-item">
-						<div class="reviews-item-img"><img src="<?=SITE_TEMPLATE_PATH?>/images/reviews1.jpg" alt=""></div>
-						<div class="reviews-item-body">
-							<div class="reviews-item-name">Кирсанов Андрей</div>
-							<div class="reviews-item-post"><span>Директор</span></div>
-							Модульное напольное покрытие из ПВХ – пример относительно недавно изобретенных материала и
-							технологии в сфере возведения
-							и отделки промышленных объектов. Такой продукт имеет множество преимуществ, благодаря которым он
-							зарекомендовал себя
-							как наиболее качественное покрытие промышленных полов.
-						</div>
-					</div>
-					<div class="reviews-item">
-						<div class="reviews-item-img"><img src="<?=SITE_TEMPLATE_PATH?>/images/reviews2.jpg" alt=""></div>
-						<div class="reviews-item-body">
-							<div class="reviews-item-name">Николай Антонов</div>
-							<div class="reviews-item-post"><span>Директор</span></div>
-							Модульное напольное покрытие из ПВХ – пример относительно недавно изобретенных материала и
-							технологии в сфере возведения
-							и отделки промышленных объектов. Такой продукт имеет множество преимуществ, благодаря которым он
-							зарекомендовал себя
-							как наиболее качественное покрытие промышленных полов.
-						</div>
-					</div>
-					<div class="reviews-item">
-						<div class="reviews-item-img"><img src="<?=SITE_TEMPLATE_PATH?>/images/reviews3.jpg" alt=""></div>
-						<div class="reviews-item-body">
-							<div class="reviews-item-name">Нечаева Виктория</div>
-							<div class="reviews-item-post"><span>Директор</span></div>
-							Модульное напольное покрытие из ПВХ – пример относительно недавно изобретенных материала и
-							технологии в сфере возведения
-							и отделки промышленных объектов. Такой продукт имеет множество преимуществ.
-						</div>
-					</div>
-				</div>
+<div class="reviews-bg">
+</div>
+<div class="wrap">
+	<h2>нас выбирают</h2>
+	<div class="reviews-block">
+		<div class="reviews-item">
+			<div class="reviews-item-img">
+ <img src="/local/templates/.default/images/reviews1.jpg" alt="">
 			</div>
-		</section>
+			<div class="reviews-item-body">
+				<div class="reviews-item-name">
+					 Строители
+				</div>
+				 <!--<div class="reviews-item-post">
+					 Директор
+				</div>--> </br>Модульное напольное покрытие из ПВХ – пример относительно недавно изобретенных материала и технологии в сфере возведения и отделки промышленных объектов. Такой продукт имеет множество преимуществ, благодаря которым он зарекомендовал себя как наиболее качественное покрытие промышленных полов.
+			</div>
+		</div>
+		<div class="reviews-item">
+			<div class="reviews-item-img">
+ <img src="/local/templates/.default/images/reviews2.jpg" alt="">
+			</div>
+			<div class="reviews-item-body">
+				<div class="reviews-item-name">
+					 Руководители и главные инженеры предприятий
+				</div>
+				 <!--<div class="reviews-item-post">
+					 Директор
+				</div>--> Модульное напольное покрытие из ПВХ – пример относительно недавно изобретенных материала и технологии в сфере возведения и отделки промышленных объектов. Такой продукт имеет множество преимуществ, благодаря которым он зарекомендовал себя как наиболее качественное покрытие промышленных полов.
+			</div>
+		</div>
+		<div class="reviews-item">
+			<div class="reviews-item-img">
+ <img src="/local/templates/.default/images/reviews3.jpg" alt="">
+			</div>
+			<div class="reviews-item-body">
+				<div class="reviews-item-name">
+					 Директора и администраторы торговых, спортивных и развлекательных центров
+				</div>
+				 <!--<div class="reviews-item-post">
+					 Директор
+				</div>--> Модульное напольное покрытие из ПВХ – пример относительно недавно изобретенных материала и технологии в сфере возведения и отделки промышленных объектов. Такой продукт имеет множество преимуществ.
+			</div>
+		</div>
+	</div>
+</div>
+ </section>
--- local/include/site_templates/hd_use.php
+++ local/include/site_templates/hd_use.php
@@ -1,43 +1,55 @@
 <section class="use-section">
-	<div id="use" class="wrap">
-		<h2>ГДЕ ИСПОЛЬЗУЕТСЯ</h2>
+<div id="use" class="wrap">
+	<h2>ГДЕ ИСПОЛЬЗУЕТСЯ</h2>
+	<div class="use-block">
 		<div class="use-block">
-			<div class="use-block">
-				<div class="use-block__line">
-					<div class="use-item">
-						<div class="use-item-img"><img src="<?=SITE_TEMPLATE_PATH?>/images/use5.png" alt=""></div>
-						Развликательные<br>
-						центры
+			<div class="use-block__line">
+				<div class="use-item">
+					<div class="use-item-img">
+ <img src="/local/templates/.default/images/use5.png" alt="">
 					</div>
-					<div class="use-item">
-						<div class="use-item-img"><img src="<?=SITE_TEMPLATE_PATH?>/images/use6.png" alt=""></div>
-						Пракинг<br>
-						Автосервис
-					</div>
-					<div class="use-item">
-						<div class="use-item-img"><img src="<?=SITE_TEMPLATE_PATH?>/images/use7.png" alt=""></div>
-						Производство<br>
-						Склады
-					</div>
+					 Развлекательные<br>
+					 центры
 				</div>
-				<div class="use-block__line use-block__line--even">
-					<div class="use-item">
-						<div class="use-item-img"><img src="<?=SITE_TEMPLATE_PATH?>/images/use8.png" alt=""></div>
-						Административные<br>
-						здания
+				<div class="use-item">
+					<div class="use-item-img">
+ <img src="/local/templates/.default/images/use6.png" alt="">
 					</div>
-					<div class="use-item">
-						<div class="use-item-img"><img src="<?=SITE_TEMPLATE_PATH?>/images/use9.png" alt=""></div>
-						Спортивные<br>
-						центры
+					 Пракинг<br>
+					 Автосервис
+				</div>
+				<div class="use-item">
+					<div class="use-item-img">
+ <img src="/local/templates/.default/images/use7.png" alt="">
 					</div>
-					<div class="use-item">
-						<div class="use-item-img"><img src="<?=SITE_TEMPLATE_PATH?>/images/use10.png" alt=""></div>
-						Торговые<br>
-						центры
+					 Производство<br>
+					 Склады
+				</div>
+			</div>
+			<div class="use-block__line use-block__line--even">
+				<div class="use-item">
+					<div class="use-item-img">
+ <img src="/local/templates/.default/images/use8.png" alt="">
 					</div>
+					 Административные<br>
+					 здания
+				</div>
+				<div class="use-item">
+					<div class="use-item-img">
+ <img src="/local/templates/.default/images/use9.png" alt="">
+					</div>
+					 Спортивные<br>
+					 центры
+				</div>
+				<div class="use-item">
+					<div class="use-item-img">
+ <img src="/local/templates/.default/images/use10.png" alt="">
+					</div>
+					 Торговые<br>
+					 центры
 				</div>
 			</div>
 		</div>
 	</div>
-</section>
+</div>
+ </section>
